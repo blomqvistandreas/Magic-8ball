9218712d7ed3332c7d2261e976136febbae205c7
andreasblomqvist <adde-16@hotmail.com>
2019-07-08 20:13:42 +0200

    Completed challenge & Finished project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
@@ -11,15 +12,47 @@ class BallPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60.0),
+        preferredSize: Size.fromHeight(70.0),
         child: AppBar(
           backgroundColor: Color(0xff558564),
-          title: Text('Ask me anything'),
+          title: Text(
+            'Ask me anything',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 50,
+                fontFamily: 'Bahianita',
+                color: Colors.black54),
+          ),
           centerTitle: false,
         ),
       ),
       backgroundColor: Color(0xff49D49D),
-      body: Container(),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: FlatButton(
+          child: Image.asset('images/ball$ballNumber.png'),
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+            print(ballNumber);
+          },
+        ),
+      ),
     );
   }
 }
