753a2acfffe913f8af74ea888d226cb2abed6cfd
andreasblomqvist <adde-16@hotmail.com>
2019-07-08 19:39:58 +0200

    Step 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60.0),
+        child: AppBar(
+          backgroundColor: Color(0xff558564),
+          title: Text('Ask me anything'),
+          centerTitle: false,
+        ),
+      ),
+      backgroundColor: Color(0xff49D49D),
+      body: Container(),
+    );
+  }
+}
